75111a188af677348db14c244124e46e0d6eb343
JuanCarlosHM <jc_worthing@ciencias.unam.mx>
2019-06-19 18:39:10 -0500

    Descripción
    Proyecto: Se creo utilizando la arquitectura MVVM. Se buscó solo utilizar código compartido. Se buscó utilizar la mayor cantidad de controladores. La persistencia se hace automaticamente con las credenciales de acceso y de manera manual con la lista de favoritos. No se moddifico el tema para que se aprecien las diferencias de la implementación en cada plataforma, pero es posible agregar tanto controladores nativos como temas a cada plataforma por separado. Se sustituyeron las cards, animaciónes y FABs con codigo compartido. Se recomienda instalar la app salir del modo depuración y realizar pruebas. El rendimiento de una app compilada con las configuraciones adecuadas es mayor que el mostrado por la actual versión. Se describen clases con código dobrante que permiten escalar la aplicación de manera más fácil.

---
+++ ExamenKokonut.Android/SplashActivity.cs
@@ -0,0 +1,20 @@
+﻿
+using Android.App;
+using Android.OS;
+
+namespace ExamenKokonut.Droid
+{
+    [Activity(Theme = "@style/theme.Splash", MainLauncher =true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            System.Threading.Thread.Sleep(0);
+            this.StartActivity(typeof(MainActivity));
+
+
+            // Create your application here
+        }
+    }
+}
---
+++ ExamenKokonut.Android/obj/Debug/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "c4445c6d-eb4b-42b8-ab66-027068496b54",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep-conc",
+
+	};
+}
